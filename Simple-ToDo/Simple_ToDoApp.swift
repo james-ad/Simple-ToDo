8a653c1e2b0b69e60580c20c22f3617a1774104f
james-du <muzikjay@gmail.com>
2023-05-31 18:51:40 -0700

    Initial Commit

---
+++ Simple-ToDo/Simple_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_ToDoApp.swift
+//  Simple-ToDo
+//
+//  Created by James Dunn on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
